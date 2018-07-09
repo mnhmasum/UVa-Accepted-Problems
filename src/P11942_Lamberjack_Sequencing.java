0106907ff6291b764b9b3e667ab5eae1c48805ab
Nazmul <mnhmasum@gmail.com>
2018-07-09 21:07:13 +0600

    P11942 Lamberjack Sequencing

---
+++ src/P11942_Lamberjack_Sequencing.java
@@ -0,0 +1,91 @@
+
+import java.util.Scanner;
+/**
+ *
+ * @author adaptive
+ */
+public class P11942_Lamberjack_Sequencing {
+
+    public static void main(String[] args) {
+        System.out.println("Lumberjacks:");
+        Scanner scanner = new Scanner(System.in);
+
+        int testCase = scanner.nextInt();
+
+        if (testCase < 20) {
+            for (int i = 0; i < testCase; i++) {
+                
+                int[] arr = new int[10]; 
+                for (int j = 0; j < 10; j++) {
+                    int speed = scanner.nextInt();
+                  
+                    arr[j] = speed;
+                }
+                
+                if (isAscOrder(arr)) {
+                    System.out.println("Ordered");
+                    continue;
+                } 
+                
+                if (isDscOrder(arr)) {
+                    System.out.println("Ordered");
+                    continue;
+                }
+                
+                System.out.println("Unordered");
+            }
+        }
+    }
+    
+    
+    public static boolean isAscOrder(int[] arr) {
+      
+        boolean isTrue = false;
+        
+        int max = arr[0];
+      
+        for (int i = 0; i < arr.length; i++) {
+            
+            if (i == 9) {
+                break;
+            }
+            
+            if (max < arr[i + 1]) {
+                isTrue = true;
+                max = arr[i + 1];
+            } else {
+                isTrue = false;
+                break;
+                
+            }  
+        }
+        
+      return isTrue;
+    }
+    
+     public static boolean isDscOrder(int[] arr) {
+      
+        boolean isTrue = false;
+        
+        int max = arr[0];
+      
+        for (int i = 0; i < arr.length; i++) {
+            
+            if (i == 9) {
+                break;
+            }
+            
+            if (max > arr[i + 1]) {
+                isTrue = true;
+                max = arr[i + 1]; 
+            } else {
+                isTrue = false;
+                break;
+                
+            }  
+        }
+        
+      return isTrue;
+    }
+    
+}
--- src/solved/P11799_Horror_Dash.java
+++ src/solved/P11799_Horror_Dash.java
@@ -2,11 +2,13 @@ package solved;
 
 
 import java.util.Scanner;
+
 /**
  *
  * @author adaptive
  */
 public class P11799_Horror_Dash {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int testCase = scanner.nextInt();
@@ -14,14 +16,14 @@ public class P11799_Horror_Dash {
         if (testCase <= 100) {
             for (int i = 0; i < testCase; i++) {
                 int students = scanner.nextInt();
-                
+
                 int minimumSpeed = 0;
                 for (int j = 0; j < students; j++) {
                     int speed = scanner.nextInt();
                     if (speed > minimumSpeed) {
                         minimumSpeed = speed;
                     }
-                    
+
                 }
 
                 System.out.println("Case " + (i + 1) + ": " + minimumSpeed);
